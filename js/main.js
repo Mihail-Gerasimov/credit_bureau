2ff88a180ffaf11a2def4cc090329719a2ea4c1a
Mihail <mihailgerasimov86@gmail.com>
2022-08-31 16:12:20 +0400

    terms

--- js/main.js
+++ js/main.js
@@ -25,95 +25,111 @@ window.onscroll = function () {
 // calc
 
 const cards = document.querySelectorAll('.calc__calc-card'),
-        payment = document.querySelector('.calc__calc-sum'),
-        select = document.querySelector('.calc__select'),
-        calcInput = document.querySelector('.calc__calc-input');
+    payment = document.querySelector('.calc__calc-sum'),
+    select = document.querySelector('.calc__select'),
+    calcInput = document.querySelector('.calc__calc-input');
 
-    function prettify(num) {
-        var n = num.toString();
-        return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + ' ');
-    }
+function prettify(num) {
+    var n = num.toString();
+    return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + ' ');
+}
 
-    function getPayment(sum, period, rate) {
-        // *
-        // * sum - сумма кредита
-        // * period - срок в годах
-        // * rate - годовая ставка в процентах
-        // * 
-        let i,
-            koef;
+function getPayment(sum, period, rate) {
+    // *
+    // * sum - сумма кредита
+    // * period - срок в годах
+    // * rate - годовая ставка в процентах
+    // * 
+    let i,
+        koef;
 
-        // ставка в месяц
-        i = (rate / 12) / 100;
+    // ставка в месяц
+    i = (rate / 12) / 100;
 
-        // коэффициент аннуитета
-        koef = (i * (Math.pow(1 + i, period * 12))) / (Math.pow(1 + i, period * 12) - 1);
+    // коэффициент аннуитета
+    koef = (i * (Math.pow(1 + i, period * 12))) / (Math.pow(1 + i, period * 12) - 1);
 
-        // итог
-        payment.textContent = (sum * koef).toFixed();
-    };
+    // итог
+    payment.textContent = (sum * koef).toFixed();
+};
 
-    function getPaymentDesktop() {
-        cards.forEach(card => {
-            if (card.classList.contains('calc__calc-card--active')) {
-                let sum = +calcInput.value.replace(/\D/g, ''),
-                    period = +card.dataset.period;
-
-                getPayment(sum, period, 5.5);
-            }
-        });
-    }
-
-    function getPaymentMobile() {
-        let sum = +document.querySelector('.calc__calc-input').value.replace(/\D/g, ''),
-            period = +select.value;
-
-        getPayment(sum, period, 5.5);
-    }
-
-    function clearActiveClass() {
-        cards.forEach(card => {
-            card.classList.remove('calc__calc-card--active');
-        });
-    }
-
+function getPaymentDesktop() {
     cards.forEach(card => {
-        card.addEventListener('click', () => {
-            clearActiveClass();
-            card.classList.add('calc__calc-card--active'); +
+        if (card.classList.contains('calc__calc-card--active')) {
+            let sum = +calcInput.value.replace(/\D/g, ''),
+                period = +card.dataset.period;
+
+            getPayment(sum, period, 5.5);
+        }
+    });
+}
+
+function getPaymentMobile() {
+    let sum = +document.querySelector('.calc__calc-input').value.replace(/\D/g, ''),
+        period = +select.value;
+
+    getPayment(sum, period, 5.5);
+}
+
+function clearActiveClass() {
+    cards.forEach(card => {
+        card.classList.remove('calc__calc-card--active');
+    });
+}
+
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        clearActiveClass();
+        card.classList.add('calc__calc-card--active'); +
             calcInput.value.replace(/\D/g, '') >= 100000 && +calcInput.value.replace(/\D/g, '') <= 20000000 ? getPaymentDesktop() : payment.textContent = '0';
-        });
     });
+});
 
-    select.addEventListener('input', () => {
-        +calcInput.value.replace(/\D/g, '') >= 100000 && +calcInput.value.replace(/\D/g, '') <= 20000000 ? getPaymentMobile() : payment.textContent = '0';
-    });
+select.addEventListener('input', () => {
+    +calcInput.value.replace(/\D/g, '') >= 100000 && +calcInput.value.replace(/\D/g, '') <= 20000000 ? getPaymentMobile() : payment.textContent = '0';
+});
 
-    calcInput.addEventListener('input', () => {
-        if (calcInput.value[0] == 0) {
-            calcInput.value = calcInput.value.replace(/./g, '');
+calcInput.addEventListener('input', () => {
+    if (calcInput.value[0] == 0) {
+        calcInput.value = calcInput.value.replace(/./g, '');
+    }
+    calcInput.value = calcInput.value.replace(/\D/g, '');
+
+    calcInput.value = prettify(calcInput.value);
+    getPaymentDesktop();
+
+    if (+calcInput.value.replace(/\D/g, '') > 20000000) {
+        calcInput.value = prettify(20000000);
+    }
+
+    if (+calcInput.value.replace(/\D/g, '') >= 100000 && +calcInput.value.replace(/\D/g, '') <= 20000000) {
+        if (select.value != '') {
+            getPaymentMobile()
         }
-        calcInput.value = calcInput.value.replace(/\D/g, '');
-
-        calcInput.value = prettify(calcInput.value);
         getPaymentDesktop();
+    } else {
+        payment.textContent = '0';
+    }
 
-        if (+calcInput.value.replace(/\D/g, '') > 20000000) {
-            calcInput.value = prettify(20000000);
-        }
-
-        if (+calcInput.value.replace(/\D/g, '') >= 100000 && +calcInput.value.replace(/\D/g, '') <= 20000000) {
-            if (select.value != '') {
-                getPaymentMobile()
-            }
-            getPaymentDesktop();
-        } else {
-            payment.textContent = '0';
-        }
-
-    });
+});
 
 
-// const swiper = new Swiper('.swiper', {
-    
-// });
+document.addEventListener('DOMContentLoaded', () => {
+    const width = window.innerWidth
+    if (width < 576) {
+        const swiper = new Swiper('.swiper', {
+
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'progressbar',
+            },
+            spaceBetween: 10,
+            slidesPerView: 1.2,
+            // loop: true,
+            // navigation: {
+            //     nextEl: '.swiper-button-next',
+            //     prevEl: '.swiper-button-prev',
+            // },
+        });
+    }
+})
